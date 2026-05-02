c6c4275b247476ac2e7a8e56f7a67ebfe30031eb
maddie.moffett <mnrmoffett@gmail.com>
2026-05-02 13:02:44 -0600

    finish classes mostly? terminate may still need some work

--- Walker.py
+++ Walker.py
@@ -8,23 +8,25 @@ class Facing:
         
         def _movelist(self):
             if self.cardinal == "North":
-                self._moves = ["up", "down", "left", "right"]
+                self._moves = ["left", "up", "right", "down"]
             elif self.cardinal == "South":
-                self._moves ["down", "up", "right", "left"]
+                self._moves ["right", "down", "left", "up"]
             elif self.cardinal == "East":
-                self._moves = ["right", "left", "up", "down"]
+                self._moves = ["up", "right", "down", "left"]
             elif self.cardinal == "West":
-                self._moves = ["left", "right", "down", "up"]
+                self._moves = ["down", "left", "up", "right"]
 
         def convert_movement(self, move_num):
             return self._movelist[move_num]
+        
+        def __str__(self):
+            return self.cardinal
 
 
 North = Facing(0)
 South = Facing(1)
 East = Facing(2)
 West = Facing(3)
-facing_order = [North, East, South, West]
 
 class Walker:
     
@@ -41,6 +43,7 @@ class Walker:
         self._facing = North                             # direction currently facing
         self._last = None                                # Previous step
         self._movelist = ["up", "down", "left", "right"] # order of movements
+        self._spinlist = [West, North, East, South]      # order of ways to face
 
     def _make_grid(self, sidelength):
         g = [li for li in [False for _ in range(sidelength)]] # make 2D grid full of False (has not been there) with sidelengths provided
@@ -65,13 +68,64 @@ class Walker:
         elif move == "right":
             return self._grid[self._gridx + 1][self._gridy]
     
-    def up(self):
+    def _move(self, objectivemove):     # helper function to ensure call of correct movement
+        if objectivemove == "up":
+            self._up()
+            self._last = "up"
+        elif objectivemove == "down":
+            self._down()
+            self._last = "down"
+        elif objectivemove == "left":
+            self._left()
+            self._last = "left"
+        elif objectivemove == "right":
+            self._right()
+            self._last = "right"
+    
+    def _up(self):
         self._y += 1     # move up a y position in graph
         self._gridy += 1 # move up a y position in GRID
         self.update()    # update the histories
+    
+    def _down(self):
+        self._y -= 1     # move down a y position in graph
+        self._gridy -= 1 # move down a y position in GRID
+        self.update()    # update the histories
+    
+    def _left(self):
+        self._x -= 1     # move down an x position in graph
+        self._gridx -= 1 # move down an x position in GRID
+        self.update()    # update the histories
+    
+    def _right(self):
+        self._x += 1     # move up an x position in graph
+        self._gridx += 1 # move up an x position in GRID
+        self.update()    # update the histories
+    
+    def _spin(self, relativemove):
+        self._facing = self._spinlist[relativemove]
+        if self._facing == North:
+            self._spinlist = [West, North, East, South]
+        elif self._facing == South:
+            self._spinlist = [East, South, West, North]
+        elif self._facing == East:
+            self._spinlist = [North, East, South, West]
+        elif self._facing == West:
+            self._spinlist = [South, West, North, East]
     
     def step(self):
         if self._last is None:
             self.up()
         else:
-            rval = int(round((random() * 2) + 1)) # generate random integer 1, 2, or 3
+            rval = int(round((random() * 2) + 1))               # generate random integer 1, 2, or 3
+            relativemove = ["left", "up", "right"][rval]        # convert to the move relative to the walker's facing
+            objectivemove = self._facing.convert_movement(rval) # convert the relative move to an objective move through the plot
+            if self.check(objectivemove):                       # if have visited the proposed spot before, terminate
+                self.terminate()
+            else:                                               # else take the step
+                self._move(objectivemove)                       # move location
+                self._spin(relativemove)                        # change direction facing
+
+
+    def terminate(self):
+        return Walker()
