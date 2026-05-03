5eb8f01ae5ce780f0b9bbe50c8a9b55085caa55c
maddie.moffett <mnrmoffett@gmail.com>
2026-05-03 01:02:44 -0600

    fix animate code and rename walker to UNweightedSAW bc that makes a lot more sense

--- Animate.py
+++ Animate.py
@@ -1,30 +1,48 @@
-import Walker
+import UNweightedSAW
 import WeightedSAW
 import pylab
 from matplotlib.animation import FuncAnimation
 
-North = Walker.Facing(0) # generate the North facing object
-South = Walker.Facing(1) # generate the South facing object
-East = Walker.Facing(2)  # generate the East facing object
-West = Walker.Facing(3)  # generate the West facing object
+North = UNweightedSAW.Facing(0) # generate the North facing object
+South = UNweightedSAW.Facing(1) # generate the South facing object
+East = UNweightedSAW.Facing(2)  # generate the East facing object
+West = UNweightedSAW.Facing(3)  # generate the West facing object
 
-def animate():
-    N = 10
+def animate(N, fname, weighted):
     ts = [i for i in range(N)]
-    sidelength = 200
-    w = WeightedSAW.Walker()
-    lastpoint = [0, 0]
+    sidelength = N*2 + 2
+    if weighted:
+        w = WeightedSAW(sidelength)
+    else:
+        w = UNweightedSAW.Walker(sidelength)
+    xs = [0]
+    ys = [0]
 
     def init():
-        pylab.plot([0], [0], ".b", ms = 1)
+        ax.plot([0], [0], ".b", ms = 1)
     
-    def update():
+    def update(frame):
         w.step()
-        pylab.plot([lastpoint[0], w._x], [lastpoint[1], w._y], ".b-", ms = 1)
-        lastpoint = [w._x, w._y]
+        xs.append(w._x)
+        ys.append(w._y)
+        art = ax.plot(xs, ys, ".b-", ms = 5, lw = 2)
+        return art
     
     fig, ax = pylab.subplots()
-    ax.set_xlim(-(sidelength//2), sidelength//2)
-    ax.set_ylim(-(sidelength//2), sidelength//2)
+    ax.set_xlim(-(N//2), N//2)
+    ax.set_ylim(-(N//2), N//2)
+    ax.set_xticks([i for i in range(-N//2, N//2)])
+    ax.set_yticks([i for i in range(-N//2, N//2)])
+    ax.grid(True)
+    fig.set_facecolor((.992,.945,.969))
 
-    ani = FuncAnimation(fig, update, init_func = init, frames = ts, interval = 1, blit = True, repeat = False)
+    ani = FuncAnimation(fig, update, init_func = init, frames = ts, interval = 400, blit = False, repeat = False)
+    ani.save("Figures/Animations/" + fname + ".gif")
+
+if __name__ == "__main__":
+    Ns = [n for n in range(10, 30, 5)]
+    fiuw = ["N" + str(s) + "-uw" for s in Ns]
+    fiw = ["N" + str(s) + "-w" for s in Ns]
+    for N in Ns:
+        animate(N, fiuw, weighted = False)
+        animate(N, fiw, weighted = True)
