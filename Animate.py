faaff17b2b3d44f4e65af5f2803667f06cc42a0a
maddie.moffett <mnrmoffett@gmail.com>
2026-05-03 02:09:36 -0600

    animate comments

--- Animate.py
+++ Animate.py
@@ -9,40 +9,40 @@ East = UNweightedSAW.Facing(2)  # generate the East facing object
 West = UNweightedSAW.Facing(3)  # generate the West facing object
 
 def animate(N, fname, weighted):
-    ts = [i for i in range(N)]
-    sidelength = N*2 + 2
-    if weighted:
+    ts = [i for i in range(N)]               # frames
+    sidelength = N*2 + 2                     # sidelength for walker grid
+    if weighted:                             # form walker object weighted or unweighted
         w = WeightedSAW.Walker(sidelength)
     else:
         w = UNweightedSAW.Walker(sidelength)
-    xs = [0]
+    xs = [0]                                 # save positional value arrays
     ys = [0]
 
-    def init():
-        ax.plot([0], [0], ".b", ms = 1)
+    def init():                              # init function for animation
+        ax.plot([0], [0], ".b", ms = 1)      # starting point
     
-    def update(frame):
-        w.step()
-        xs.append(w._x)
-        ys.append(w._y)
+    def update(frame):                       # update at each frame
+        w.step()                             # take a step
+        xs.append(w._x)                      # save the x and y vals
+        ys.append(w._y)                      # then plot all of the steps:
         art = ax.plot(xs, ys, ".b-", ms = 5, lw = 2)
         return art
     
-    fig, ax = pylab.subplots()
-    ax.set_xlim(-(N//2), N//2)
-    ax.set_ylim(-(N//2), N//2)
+    fig, ax = pylab.subplots()               # initialize plot
+    ax.set_xlim(-(N//2), N//2)               # bounds of graph
+    ax.set_ylim(-(N//2), N//2)               # gridlines:
     ax.set_xticks([i for i in range(-N//2, N//2)])
     ax.set_yticks([i for i in range(-N//2, N//2)])
     ax.grid(True)
-    fig.set_facecolor((.992,.945,.969))
+    fig.set_facecolor((.992,.945,.969))      # pale pink background for slay
 
-    ani = FuncAnimation(fig, update, init_func = init, frames = ts, interval = 400, blit = False, repeat = False)
-    ani.save("Figures/Animations/" + fname + ".gif")
+    ani = FuncAnimation(fig, update, init_func = init, frames = ts, interval = 400, blit = False, repeat = False) # animation call!
+    ani.save("Figures/Animations/" + fname + ".gif") # save using given filename center
 
 if __name__ == "__main__":
-    Ns = [n for n in range(10, 30, 5)]
-    fiuw = ["N" + str(s) + "-uw" for s in Ns]
-    fiw = ["N" + str(s) + "-w" for s in Ns]
-    for i in range(len(Ns)):
-        animate(Ns[i], fiuw[i], weighted = False)
-        animate(Ns[i], fiw[i], weighted = True)
+    Ns = [n for n in range(10, 30, 5)]               # N vals we're graphing
+    fiuw = ["N" + str(s) + "-uw" for s in Ns]        # file names for unweighted
+    fiw = ["N" + str(s) + "-w" for s in Ns]          # file names for weighted
+    for i in range(len(Ns)):                         # iterate through
+        animate(Ns[i], fiuw[i], weighted = False)    # animate unweighted version at this N
+        animate(Ns[i], fiw[i], weighted = True)      # animate weighted version at this N
